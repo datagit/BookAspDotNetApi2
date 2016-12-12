d5bf9eb79b5369b7fa5f97a5efab6ddad5b23305
dat.dao <dat.dao@PC-DATDAO>
2016-12-12 11:11:57 +0700

    Add project files.

---
+++ BookAspDotNetApi2/Models/Author.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookAspDotNetApi2.Models {
+    public class Author {
+        public int AuthorId { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+}
